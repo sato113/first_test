1f4383b486be3cbc1af1882bfe6a6c765e16b613
satoshi <s.inoue.113@gmail.com>
2023-01-04 13:20:24 +0900

    ページ内リンクのスムーススクロール作成

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    //ページ内リンクのスムーススクロール
+    $('a[href^="#"]').click(function(){
+        const speed = 500;
+        const href =$(this).attr("href");
+        const target = $(href === "#" || href === "" ? 'html' : href);
+        const position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
