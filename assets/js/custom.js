2b35a41b10d8958a53ca109793b58dbf474c4aa6
satoshi <s.inoue.113@gmail.com>
2023-01-14 17:17:19 +0900

    ページトップスクロール

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,4 +8,21 @@ $(document).ready(function(){
         $("html, body").animate({scrollTop:position}, speed, "swing");
         return false;
     });
+
+     //ページトップスクロール
+    const pagetop = $('.page_top');
+     // ボタン非表示
+        pagetop.hide();
+    // 100px スクロールしたらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
 });
